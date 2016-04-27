1057aa909dc6e8a0694b33f51dec1bbc3d893adc
pilot <lyotchik1@mail.ru>
2016-04-27 17:45:01 +0300

    добавление основных страниц

--- wedding/diamond/views.py
+++ wedding/diamond/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'diamond/index.html', {})
+
+def about(request):
+    return render(request, 'diamond/about.html', {})
+
+def contacts(request):
+    return render(request, 'diamond/contacts.html', {})
+
+def testimonials(request):
+    return render(request, 'diamond/testimonials.html', {})
